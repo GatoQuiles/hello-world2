e22765e9274ee328bf850c96f6662b29cc1a762c
GatoQuiles <jrquiles11@gmail.com>
2025-09-09 18:31:30 +0200

    sa subio?

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,12 @@ print ('hello world')
 #CREEEMEEEEEEEE
 print ('HolaGaato')
 print ("pull")
+f = open("holajohn", "x")
+f = open("holajohn", "w")
+j = ["Hola mozo \n", "Toma tu archivo perro \n"]
+f.write("Holi \n")
+f.writelines(j)
+f.close()
+f = open("holajohn", "r")
+print (f.readline())
+print (f.readline())
