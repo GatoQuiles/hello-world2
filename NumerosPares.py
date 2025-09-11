844f2b351de54ee90e18b903c44557340d396443
skizojohn <johnsuarez86@gmail.com>
2025-09-11 18:34:33 +0200

    ejercicio nuevo

---
+++ NumerosPares.py
@@ -0,0 +1,5 @@
+#Crear un programa en esta hoja que reciba un numero cualquiera por consola, guarde el numero en una variable 
+#e imprima todos los numero pares que hay entre 0 y el numero dado por consola
+#por ejemplo si recibimos 10  el programa tendra que imprimir 2 4 6 8 .
+#si le damos 15 tendra que imprimir 2 4 6 8 10 12 14
+#si le damos 1 no imprimira nada y asi sucesivamente
