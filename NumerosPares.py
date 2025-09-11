a012a43b4625693281ffe08d39baff22a8eaf101
GatoQuiles <jrquiles11@gmail.com>
2025-09-11 19:30:37 +0200

    los numeritos

--- NumerosPares.py
+++ NumerosPares.py
@@ -4,3 +4,7 @@
 #si le damos 15 tendra que imprimir 2 4 6 8 10 12 14
 #si le damos 1 no imprimira nada y asi sucesivamente
 #Buscar en google como saber si un numero es par"
+z=int(input("ingresar numero: "))
+print(f"Números pares entre 0 y {z} son:")
+for i in range(z+1):
+    if i %2==0:print(i)
