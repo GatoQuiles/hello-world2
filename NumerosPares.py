1cefc8991626c3bd9821698be5e9f8a5b8defa0d
skizojohn <johnsuarez86@gmail.com>
2025-09-11 18:50:10 +0200

    ejercicio pares

--- NumerosPares.py
+++ NumerosPares.py
@@ -3,3 +3,4 @@
 #por ejemplo si recibimos 10  el programa tendra que imprimir 2 4 6 8 .
 #si le damos 15 tendra que imprimir 2 4 6 8 10 12 14
 #si le damos 1 no imprimira nada y asi sucesivamente
+#Buscar en google como saber si un numero es par"
--- ReadFileWithOpen.py
+++ ReadFileWithOpen.py
@@ -1,8 +1,15 @@
 #Crear un archivo con extension txt y abrirlo con with open 
 #https://www.freecodecamp.org/news/with-open-in-python-with-statement-syntax-example/
-archivo=open("archivo11.txt","x")
+#archivo=open("archivo11.txt","x")
 archivo=open("archivo11.txt","w")
 A=["Acagar quijote \n","que paso \n"]
 archivo.writelines(A)
-with open("archivo11.txt") as archivo:
-    print(archivo.read())
+
+#with open("archivo11.txt") as archivo:
+#    print(archivo.read())
+
+
+#cuando usamos un "as" en python, estamos cargando una libreria, direccion, archivo, operacion etc... dentro de la variable
+#Que sigue a "as"
+with open ("archivo11.txt") as pene:
+    print (pene.read())
