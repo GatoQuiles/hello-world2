f3f02afe5be451eb926bba4f5ddff5c0ba778020
GatoQuiles <jrquiles11@gmail.com>
2025-09-16 16:05:06 +0200

    Input y mates

--- input.py
+++ input.py
@@ -1,2 +1,4 @@
 #Necesitamos que entre un texto cualquiera por teclado e imprimirlo en la misma consola.
 #Usar clase input
+x=input("ingresar texto: ")
+print(f"Hola John {x}: ")
--- mates.py
+++ mates.py
@@ -1,2 +1,6 @@
 # Un programa que sume dos numeros que pongas en variables distintas y que nos muestre el resultado
 # Las variabels las tienes que poner en el programa, no recogerlas por teclado
+x=6
+y=4
+suma=x+y
+print(f"La suma es: {suma}")
